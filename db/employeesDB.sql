6d5d4cfc20b87e3eee21362b3e1469528fe9bbe6
unknown <pak.phil7@gmail.com>
2020-05-03 18:27:50 -0700

    inserted code for adding new department and viewing all employees

--- db/employeesDB.sql
+++ db/employeesDB.sql
@@ -11,7 +11,7 @@ PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-id INT,
+id INT AUTO_INCREMENT,
 title VARCHAR(30),
 salary DECIMAL(10,2),
 department_id INT,
@@ -20,7 +20,7 @@ FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-id INT,
+id INT AUTO_INCREMENT,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
